a65db16958fe92746cf65762e6250582083f8a1f
seray keskin <ksknsry_52@hotmail.com>
2020-04-19 22:08:09 +0300

    change

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -32,7 +32,12 @@ namespace WebApi
             services.AddSingleton<IBookService, BookManager>();
             services.AddSingleton<IBookDal, EFBookDal>();
             services.AddMvc();
-        
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("http://localhost:4201"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,9 +52,10 @@ namespace WebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4201").AllowAnyHeader());
             app.UseMvc();
+           
         }
     }
 }
